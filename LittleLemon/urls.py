b4036d319045cefd5fdacf2445882c7c59846db4
muwagamicheal <michealmuwaga@gmail.com>
2023-05-10 17:13:02 +0300

    add imports need to use djoser urls
    added the import in line 18 to enable the use of urls in the paths list as required by djoser

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
+
 
 #simple jwt imports
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
 
 urlpatterns = [
