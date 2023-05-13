c5e43dbee93f7419a056c6ff6b95bf44f6bcd384
muwagamicheal <michealmuwaga@gmail.com>
2023-05-13 10:22:04 +0300

    added authentication class
    at the project level, that allows only users with a valid token to access the list of menu items

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -136,14 +136,21 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.permissions.IsAuthenticated', # this is used for prject level permissions to authenticated users.
+        #'rest_framework_simplejwt.authentication.JWTAuthentication',
         # token based authentication
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        #'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_FILTER_BACKENDS':[
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3
 }
 
 SIMPLE_JWT = {
