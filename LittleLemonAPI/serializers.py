3e8d5994c940ea2f485a770d18315f48630bdb4a
muwagamicheal <michealmuwaga@gmail.com>
2023-05-15 19:40:04 +0300

    added category
    class in the serializer and view

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
-from .models import MenuItem
+from .models import MenuItem, ItemCategory
 
 # Serializer classes
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemCategory
+        fields = ['__all__']
 
 class MenuItemSerializer(serializers.ModelSerializer):
+    category_id = serializers.IntegerField(read_only=True)
+    #item_category = CategorySerializer(write_only=True)
     class Meta:
         model = MenuItem
-        fields = ['id','item_name','price']
+        fields = ['id','item_name','price','category_id','item_category',]
+
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,18 +1,23 @@
 #from django.shortcuts import render
 from rest_framework import generics
-from .models import MenuItem
-from .serializers import MenuItemSerializer
+from .models import MenuItem, ItemCategory
+from .serializers import MenuItemSerializer, CategorySerializer
 
 # 
-from rest_framework.views import APIView
+#from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 #from rest_framework import permissions
 
 # Create your views here.
 
+#Used to retrieve all categories in the database
+class CategoryView(generics.ListCreateAPIView):
+    queryset = ItemCategory.objects.all()
+    serializer_class = CategorySerializer
+
  # Used to retrieve all menu items in the database
-class MenuItemView(generics.ListCreateAPIView):
+class MenuItemView(generics.ListCreateAPIView, generics.UpdateAPIView, generics.DestroyAPIView):
     permission_classes = (IsAuthenticated,)
 
     queryset = MenuItem.objects.all()
@@ -20,8 +25,13 @@ class MenuItemView(generics.ListCreateAPIView):
     ordering_fields = ['item_name','price']
     filtering_fields = ['price']
     search_fields = ['item_name']
-"""
+
     def get(self, request):
+        queryset = self.get_queryset()
+        results = self.serializer_class(queryset, many=True).data    
+        return Response(results)
+
+        """
         if self.has_permission(request, self):
             queryset = self.get_queryset()
             results = self.serializer_class(queryset, many=True).data
@@ -30,4 +40,23 @@ class MenuItemView(generics.ListCreateAPIView):
         else:
             # Do something else if user does not have permission
             return Response('User does not have permission')
-"""
+        """
+
+    def post(self, request):
+        serializer = self.get_serializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data)
+
+        """
+
+        
+        if self.has_permission(self, request):
+            serializer = self.get_serializer(data = request.data)
+            serializer.is_valid(raise_exception = True)
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_202_CREATED)
+        else:
+            return Response('User does not have permission to create menu item')
+        """
+            
