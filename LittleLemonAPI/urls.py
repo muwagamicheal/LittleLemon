223d2d287994ee883153a2a9dd008731590b26a0
muwagamicheal <michealmuwaga@gmail.com>
2023-05-15 19:38:59 +0300

    clarified the app
    from which to pick the models used by the serializer

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,6 +1,6 @@
 #from django.contrib import admin
 from django.urls import path
-from . import views
+from LittleLemonAPI import views
 #from django.conf.urls import url
 
 ## User registration andtoken generation endpoins
